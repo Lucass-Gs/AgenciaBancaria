786cb5bb65586903c80e91c3ce1eaa2f70581dcf
Lucas Santos <lucasgabriel153@gmail.com>
2021-09-23 15:15:14 -0300

    Projeto utilizando os 4 pilares da POO

---
+++ AgenciaBancaria.app/Program.cs
@@ -0,0 +1,42 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Endereco endereco = new Endereco("RUA TESTE", "134565", "PERDERNEIRAS", "SP");
+                Cliente cliente = new Cliente("Carla", "12345646", "11111", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta " +conta.Situacao + ": "  + conta.NumeroConta + "-" + 
+                    conta.DigitoVerificador);
+                string senha = "abc123456";
+                conta.Abrir(senha);
+                
+
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumeroConta + "-" +
+                    conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Saldo:" + conta.Saldo);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+    }
+}
